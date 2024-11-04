52298472fc91aa224e53fee3f42d5319fdde2c9b
Adnaan Janees <adnaanjanees0@gmail.com>
2024-11-04 20:01:00 +0530

    SpecialtyMenu done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Headers from '../components/Headers'
+import SpecialtyMenu from '../components/SpecialtyMenu'
 
 const Home = () => {
   return (
     <div>
         <Headers />
+        <SpecialtyMenu />
     </div>
   )
 }
