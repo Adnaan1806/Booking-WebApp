0bdfb2888b4f9fd37064bdf5dc200d5d247e4f58
Adnaan Janees <adnaanjanees0@gmail.com>
2024-11-04 20:06:49 +0530

    Top Doctors page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Headers from '../components/Headers'
 import SpecialtyMenu from '../components/SpecialtyMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
         <Headers />
         <SpecialtyMenu />
+        <TopDoctors />
     </div>
   )
 }
