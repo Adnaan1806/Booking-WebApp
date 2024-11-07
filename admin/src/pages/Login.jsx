6e658339ac723e9cbf9a83294f3e4d7038a32100
Adnaan Janees <adnaanjanees0@gmail.com>
2024-11-07 23:37:52 +0530

    logout functionality done

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -18,11 +18,12 @@ const Login = () => {
         if(state === "Admin"){
            const {data} = await axios.post(backendUrl + '/api/admin/login', {email, password});
            if(data.success){
-            localStorage.setItem("aToken", data.token);
+            localStorage.setItem("aToken", data.token); //ADMIN_EMAIL = "admin@timely.com"
+                                                        //ADMIN_PASSWORD = "admin12345"
                setAToken(data.token);
            }
            else{
-             toast.error(data.message);
+             toast.error(data.message);    
            }
         }
         else{
